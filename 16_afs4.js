d9cf67dcb547828b44b198118164a4f1f0dbcf6b
aaryapatel1586 <aaryapatel1586@gmail.com>
2026-03-16 10:24:10 +0530

    Add files via upload

---
+++ 16_afs4.js
@@ -0,0 +1,15 @@
+var fs = require("fs");
+fs.writeFile("help.txt", "You are creating the file", (e) => {
+    if (e) throw e;
+    else {
+        fs.appendFile("help.txt", "\n You are appending the data", (e) => {
+            if (e) throw e;
+            else {
+                fs.readFile("help.txt", "utf-8", (e, data) => {
+                    if (e) throw e;
+                    console.log("Thank you for using my program");
+                })
+            }
+        });
+    }
+});
